7180d42281cbae68281e167520a40344b72917f3
Bradley Morrison <45197952+brad-morrison@users.noreply.github.com>
2022-12-22 01:43:28 +0000

    Added DOTween events and tested them

--- Assets/Scripts/GameView.cs
+++ Assets/Scripts/GameView.cs
@@ -96,23 +96,12 @@ public class GameView : GameElement
 
     public void OpenGameOverUI() {
         game.model.gameOverUI_Group.active = true;
-
+        // set game over ui scores
         game.model.finalScore.GetComponent<TextMeshPro>().text = game.model.score.ToString();
         game.model.finalBest.GetComponent<TextMeshPro>().text = game.model.highScore.ToString();
 
-
-        Sequence OpenGameOverUI_seq = DOTween.Sequence();
+        // run FSM here
         
-        // move in top and bottom UI
-        OpenGameOverUI_seq
-            .Append(game.model.gameOverUI_top.transform.DOMoveY(1.4f, 1f).From().SetEase(Ease.OutBounce))
-            .Insert(0.2f, game.model.gameOverUI_buttons.transform.DOMove(new Vector3(0,-10,0), 1).From().SetEase(Ease.OutQuint))
-        // move in yeti and hiker
-            .Insert(0.4f, game.model.gameOverUI_yeti.transform.DOMoveY(-5f, 1).From().SetEase(Ease.OutQuint))
-            .Insert(0.6f, game.model.gameOverUI_hiker.transform.DOMoveY(1.8f, 1).From().SetEase(Ease.OutBounce))
-        // wait and change sprites
-            .InsertCallback(2f, ()=>ChangeSprite(game.model.gameOverUI_hiker, game.model.hiker_red_smiling));
-
     }
 
     public void ChangeSprite(GameObject obj, Sprite sprite) {
