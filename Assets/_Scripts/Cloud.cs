cb68f785a2a4ada600f57c318bdda6cf0daf331d
brad-morrison <bradley_morrison@outlook.com>
2023-05-01 23:41:32 +0100

    Update Cloud.cs

--- Assets/_Scripts/Cloud.cs
+++ Assets/_Scripts/Cloud.cs
@@ -14,7 +14,9 @@ public class Cloud : MonoBehaviour
 
     private void Update()
     {
+        // move cloud every frame
         transform.position = new Vector2(transform.position.x + step, transform.position.y);
+        // destroy if out of bounds
         if (transform.position.x < -1.8f) transform.position = new Vector2(1.8f, transform.position.y);
     }
 }
